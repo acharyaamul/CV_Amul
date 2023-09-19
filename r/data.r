d74c27c70599c82c67cfeb5cbe0bf1ef9616e98d
acharyaamul <acharya.amulkumar@gmail.com>
2023-09-19 14:04:52 +0545

    Third Commit

--- r/data.r
+++ r/data.r
@@ -1,16 +1,16 @@
 work <- tribble(
     ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "Develop research protocols and conduct forest research",
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "Conduct forest inventory and data analysis",
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "Make and update National Land Cover Monitoring System of Nepal",
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "prepapre Activity data (Forest Degradation and Deforestation) for REDD plus program using Remote Sensing and GIS techniques",
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "Facilitate on training program for the Forestry Professional ",
-    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Research/Remote Sensing Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
     "Supervise the Bachelors and Masters students of Forestry for their project work ",
     
     "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
@@ -42,20 +42,6 @@ edu <- tribble(
     "Tribhuwan University", 1999, 2001, "Technical Certificate in Forestry", "Hetauda, Nepal",NA
 )
 
-trainings<- tribble(
-    ~area, ~accomplishment, ~year, ~where, ~detail,
-    "Certified Lean Six Sigma Green Belt", NA, "", "", NA,
-    "Certified Lean Six Sigma White Belt", NA, "", "", NA,
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Excel - 112",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Word - 22",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Outlook - 21",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Power Point - 18",
-    "Microsoft Software", "Various Applications", "", "", "Share Point - 9",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Project - 6",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Access - 3",
-    "Microsoft Software", "Various Applications", "", "", "Microsoft Power BI - 3"
-)
-
 skills <- tribble(
     ~area, ~skills,
     "Programming Languages", "R, Python, JavaScript",
@@ -68,7 +54,7 @@ skills <- tribble(
 honors <- tribble(
     ~area, ~accomplishment, ~year, ~where, ~detail,
     "DAAD", "DAAD scholarship for master’s degree", 2017, "George August University, Germany",NA,
-    "Government Of Nepal ", "Scholarship for BSc Forestry", 2005, "Tribhuwan University, Nepal",NA
+    "Government of Nepal ", "Scholarship for BSc Forestry", 2005, "Tribhuwan University, Nepal",NA
 )
 
 
