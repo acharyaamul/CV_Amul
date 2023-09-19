ccb6dc3a5c1b99934d96c3b1484e1ae0f171684a
acharyaamul <acharya.amulkumar@gmail.com>
2023-09-19 09:31:06 +0545

    first commit

---
+++ r/data.r
@@ -0,0 +1,78 @@
+work <- tribble(
+    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Develop research protocols and conduct forest research",
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Conduct forest inventory and data analysis",
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Make and update National Land Cover Monitoring System of Nepal",
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "prepapre Activity data (Forest Degradation and Deforestation) for REDD plus program using Remote Sensing and GIS techniques",
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Facilitate on training program for the Forestry Professional ",
+    "Remote Sensing/Research Officer", "Forest Research and Training Centre", "July", 2013, "Present", NA, "Babarmahal,Kathmadu", 
+    "Supervise the Bachelors and Masters students of Forestry for their project work ",
+    
+    "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
+    "Conservation and Management of Forest Resources",
+    "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
+    "Assist on preparation and revision of Forest Management Plan ofCommunity Forests and Leasehold Forests",
+    "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
+    "Facilitate on trainings and workshops related to Forest Management, Non Timber Forest Products (NTFPs), Climate Change
+    and Gender issues in Community Forest User Groups and Leasehold Forest User Groups",
+    "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
+    "Assist on Nursery establishment, seedling production & plantations",
+    "Ranger", "District Forest Officer (Department of Forest) ", "November", 2010, "July", 2013, "Makawanpur,Nepal",
+    "Support District Forest Officer for planning and monitoring of development activities conducted by Range post staffs",
+    
+    " Consultant Ranger", "Federation of Community Forestry Users, Nepal(FECOFUN) ", "January", 2002, "July", 2005, "Makawanpur,Nepal",
+    "Assist on Forest user group formation",
+    " Consultant Ranger", "Federation of Community Forestry Users, Nepal(FECOFUN) ", "January", 2002, "July", 2005, "Makawanpur,Nepal",
+    "Assist on preparation and revision of Operation Plan of Community Forest",
+    " Consultant Ranger", "Federation of Community Forestry Users, Nepal(FECOFUN) ", "January", 2002, "July", 2005, "Makawanpur,Nepal",
+    "Identify and empower women and disadvantage groups",
+    " Consultant Ranger", "Federation of Community Forestry Users, Nepal(FECOFUN) ", "January", 2002, "July", 2005, "Makawanpur,Nepal",
+    "Facilitation on training and workshops related to Community Forestry"
+    
+)
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+    "George August University", 2017, 2019, "M.Sc. in Forest Sciences and Forest Ecology", "Goettingen,Germany",NA,
+    "Tribhuwan University", 2005, 2009, "Bachelors' Degree in Forestry", "Hetauda, Nepal",NA,
+    "Tribhuwan University", 1999, 2001, "Technical Certificate in Forestry", "Hetauda, Nepal",NA
+)
+
+trainings<- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Certified Lean Six Sigma Green Belt", NA, "", "", NA,
+    "Certified Lean Six Sigma White Belt", NA, "", "", NA,
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Excel - 112",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Word - 22",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Outlook - 21",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Power Point - 18",
+    "Microsoft Software", "Various Applications", "", "", "Share Point - 9",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Project - 6",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Access - 3",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Power BI - 3"
+)
+
+skills <- tribble(
+    ~area, ~skills,
+    "Programming Languages", "R, Python, JavaScript",
+    "Markup Languages", "RMarkdown, Markdown",
+    "Text Editors", "RStudio,Jupyter Notebook,Google Colab,Visual Studio Code",
+    "Remote Sensing/GIS","QGIS, Google Earth Engine, SNAP, CloudCompare, Fusion, LAStools, ArcGIS, ArcGIS Pro, ERDAS",
+    "Others","MS Office( Excel, Outlook, PowerBI, PowerPoint,Word),SPSS,Zotero etc."
+)
+
+honors <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "DAAD", "DAAD scholarship for master’s degree", 2017, "George August University, Germany",NA,
+    "Government Of Nepal ", "Scholarship for BSc Forestry", 2005, "Tribhuwan University, Nepal",NA
+)
+
+
+
+
+
+
